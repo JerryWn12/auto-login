3641fbc34936728fd138fb6f97b28b137d3f7db1
JerryWn <2571290541qq@gmail.com>
2024-03-30 18:57:12 +0800

    replace interface detection method

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Usage
 
-1. Create 3 virtual lan interface(vth0, vth1, vth2) on router with macvlan.
-2. Generate `numbers.txt` file: `python generate_numbers.py`
+1. Create virtual interface on router with macvlan.
+2. Edit `interfaces` variable in `run.sh` to your interface.
+3. Generate `numbers.txt` file: `python generate_numbers.py`
 
-3. ```shell script
+4. ```shell script
    mv host.txt.example host.txt
    mv account_prefix.txt.example account_prefix.txt
    mv password.txt.example password.txt
    ```
-4. Configure 3 files above with example files.
-5. `bash login.sh`
+5. Configure 3 files above with example files.
+6. `bash run.sh`
 
 tips
 > Lively view log file with `tail -f log.txt`, successfully scanned account will be stored in `success.txt`.
--- run.sh
+++ run.sh
@@ -4,12 +4,20 @@ log_file_name="log.txt"
 numbers_file_name="numbers.txt"
 success_number_file_name="success.txt"
 
-current_if=""
-
 host=$(cat host.txt)
 account_prefix=$(cat account_prefix.txt)
 password=$(cat password.txt)
 
+interfaces=(
+    "eth0"
+    "vth0"
+    "vth1"
+    "vth2"
+    "vth3"
+    "vth4"
+    "vth5"
+)
+current_if=""
 
 log() {
     current_time=$(date "+%Y/%m/%d %T")
@@ -22,32 +30,18 @@ log_success () {
 }
 
 choose_if() {
-    log "choosing interface to login"
-    response_vth0=$(curl "www.baidu.com" -s --interface "vth0")
-    response_vth1=$(curl "www.baidu.com" -s --interface "vth1")
-    response_vth2=$(curl "www.baidu.com" -s --interface "vth2")
-    if [ -n "$response_vth0" ]; then
-        log "vth0 up"
-    else
-        log "vth0 down, ready to login"
-        current_if="vth0"
-        return
-    fi
-    if [ -n "$response_vth1" ]; then
-        log "vth1 up"
-    else
-        log "vth1 down, ready to login"
-        current_if="vth1"
-        return
-    fi
-    if [ -n "$response_vth2" ]; then
-        log "vth2 up"
-    else
-        log "vth2 down, ready to login"
-        current_if="vth2"
-        return
-    fi
-    log "all interfaces up"
+    log "Choosing interface to login."
+    for interface in "${interfaces[@]}"; do
+        is_logged_in=$(bash get_interface_info.sh "$interface" | grep "yes")
+        if [ -n "$is_logged_in" ]; then
+            log "Interface $interface up."
+        else
+            log "Interface $interface down, ready to login."
+            current_if="$interface"
+            return
+        fi
+    done
+    log "All interfaces up."
     exit 0
 }
 
@@ -58,7 +52,7 @@ login() {
         -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
         -H 'Accept-Language: zh-CN,zh;q=0.9' \
         -H 'Connection: keep-alive' \
-        -H 'Referer: http://'$host'/a70.htm?isReback=1' \
+        -H 'Referer: http://'"$host"'/a70.htm?isReback=1' \
         -H 'Upgrade-Insecure-Requests: 1' \
         -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' \
         -i -s --interface "$3"
