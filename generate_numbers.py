8808f525e5d174bce64413c721fd01456d04644c
JerryWn <2571290541qq@gmail.com>
2024-03-31 15:02:01 +0800

    use unix line ending

--- generate_numbers.py
+++ generate_numbers.py
@@ -4,5 +4,5 @@ numbers = list(range(10000))
 
 random.shuffle(numbers)
 
-with open("numbers.txt", mode="a") as file:
+with open("numbers.txt", mode="a", newline="\n") as file:
     file.write("\n".join(str(f"{number:04d}") for number in numbers))
